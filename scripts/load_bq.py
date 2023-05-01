GoogleCloudPlatform/slurm-gcp

commit 913411dffe11db7bd788e7353254bfc80abbc95e

Fix unused edge case in load_bq.py
No behavior change.

scripts/load_bq.py
@@ -22,7 +22,7 @@ SACCT = "sacct"
 script = Path(__file__).resolve()
 
 DEFAULT_TIMESTAMP_FILE = script.parent / "bq_timestamp"
-timestamp_file = os.environ.get("TIMESTAMP_FILE") or DEFAULT_TIMESTAMP_FILE
+timestamp_file = Path(os.environ.get("TIMESTAMP_FILE")) or DEFAULT_TIMESTAMP_FILE
 
 # cluster_id_file = script.parent / 'cluster_uuid'
 # try:

commit 971ee26e79793e1974f2fdfc5463685eade95660

Fix regression in load_bq.py
regression in commit 913411dffe11db7bd788e7353254bfc80abbc95e.

scripts/load_bq.py
@@ -22,7 +22,7 @@ SACCT = "sacct"
 script = Path(__file__).resolve()
 
 DEFAULT_TIMESTAMP_FILE = script.parent / "bq_timestamp"
-timestamp_file = Path(os.environ.get("TIMESTAMP_FILE")) or DEFAULT_TIMESTAMP_FILE
+timestamp_file = Path(os.environ.get("TIMESTAMP_FILE", DEFAULT_TIMESTAMP_FILE))
 
 # cluster_id_file = script.parent / 'cluster_uuid'
 # try: