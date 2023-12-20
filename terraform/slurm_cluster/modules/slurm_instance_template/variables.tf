9ec63076881fe8aec49cd0f89f4c0c75e8366149
Ivan Orlov <orlov@google.com>
2023-12-20 06:25:37 +0000

    Don't set networkIntefaces in script, surface `access_config`

--- terraform/slurm_cluster/modules/slurm_instance_template/variables.tf
+++ terraform/slurm_cluster/modules/slurm_instance_template/variables.tf
@@ -139,6 +139,15 @@ variable "additional_networks" {
   }))
 }
 
+variable "access_config" {
+  description = "Access configurations, i.e. IPs via which the VM instance can be accessed via the Internet."
+  type = list(object({
+    nat_ip       = string
+    network_tier = string
+  }))
+  default = []
+}
+
 ############
 # INSTANCE #
 ############
