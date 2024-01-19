80a071e94633f1cd5472704a1b33338883f07b15
Carson Dunbar <carsondunbar@google.com>
2024-01-19 15:02:37 +0000

    Fixing to account for clusters with no login nodes

--- scripts/setup.py
+++ scripts/setup.py
@@ -591,7 +591,7 @@ def install_custom_scripts(clean=False):
 def run_custom_scripts():
     """run custom scripts based on instance_role"""
     custom_dir = dirs.custom_scripts
-    suffix = instance_metadata("attributes/slurm_login_suffix")
+    suffix = None
     if lkp.instance_role == "controller":
         # controller has all scripts, but only runs controller.d
         custom_dirs = [custom_dir / "controller.d"]
@@ -600,6 +600,7 @@ def run_custom_scripts():
         custom_dirs = [custom_dir / "compute.d", custom_dir / "partition.d"]
     elif lkp.instance_role == "login":
         # login setup with only login_{suffix}.d
+        suffix = instance_metadata("attributes/slurm_login_suffix")
         custom_dirs = [custom_dir / f"login_{suffix}.d"]
     else:
         # Unknown role: run nothing
@@ -619,9 +620,8 @@ def run_custom_scripts():
                 timeout = lkp.cfg.get("controller_startup_scripts_timeout", 300)
             elif "/compute.d/" in str(script):
                 timeout = lkp.cfg.get("compute_startup_scripts_timeout", 300)
-            elif "/login.d/" in str(script):
-                timeout = lkp.cfg.get("login_startup_scripts_timeout", 300)
-            elif f"/login_{suffix}.d/" in str(script):
+            elif "/login.d/" in str(script) or \
+                 (suffix is not None and f"/login_{suffix}.d/" in str(script)):
                 timeout = lkp.cfg.get("login_startup_scripts_timeout", 300)
             elif "/partition.d/" in str(script):
                 partition_name = lkp.node_partition_name()
