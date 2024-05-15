ba65ff57b279a4ab9ce1aba8b1141d622295c436
Tom Downes <tpdownes@google.com>
2024-05-15 10:30:31 -0500

    Update image versions to 5.11 release

--- terraform/slurm_cluster/modules/slurm_instance_template/main.tf
+++ terraform/slurm_cluster/modules/slurm_instance_template/main.tf
@@ -51,7 +51,7 @@ locals {
   source_image_family = (
     var.source_image_family != "" && var.source_image_family != null
     ? var.source_image_family
-    : "slurm-gcp-5-10-hpc-centos-7"
+    : "slurm-gcp-5-11-hpc-centos-7"
   )
   source_image_project = (
     var.source_image_project != "" && var.source_image_project != null
