8618ac0cf6d5a393556b1c077bcb0825c2ea82a0
Tom Downes <tpdownes@google.com>
2024-10-29 04:10:09 +0000

    Update slurm_controller_instance to be TPG v6 compatible

--- terraform/slurm_cluster/modules/slurm_controller_instance/versions.tf
+++ terraform/slurm_cluster/modules/slurm_controller_instance/versions.tf
@@ -20,7 +20,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.84, < 6.0"
+      version = ">= 4.84"
     }
     local = {
       source  = "hashicorp/local"
