5bc6124218244f8f353c38faef3054c3a8fa9b2c
abbas1902 <abbasmohamed@google.com>
2025-02-27 18:31:39 +0000

    Upgrade to Slurm version 24.11.2

--- packer/docker/build.py
+++ packer/docker/build.py
@@ -107,7 +107,7 @@ parser.add_argument(
     "--slurm_version",
     "-s",
     dest="slurm_version",
-    default="24.05.5",
+    default="24.11.2",
     help="The Slurm version to use for the image.",
 )
 parser.add_argument(
