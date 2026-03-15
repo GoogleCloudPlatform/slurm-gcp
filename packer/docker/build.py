63c9f82065f0ec6018e36fafde63f8521d0e5135
Adarsh Kullachi <adarshkullachi@google.com>
2026-03-15 08:54:38 +0000

    Upgrade Slurm to 25.11.3

--- packer/docker/build.py
+++ packer/docker/build.py
@@ -107,7 +107,7 @@ parser.add_argument(
     "--slurm_version",
     "-s",
     dest="slurm_version",
-    default="25.11.2",
+    default="25.11.3",
     help="The Slurm version to use for the image.",
 )
 parser.add_argument(
